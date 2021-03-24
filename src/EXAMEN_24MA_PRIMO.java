a48a3693503e4941747db23eb7e35cf88f5203e6
CARLITOS <CARLITOS@DESKTOP-DN009DA>
2021-03-24 11:23:48 +0100

    Primero

---
+++ src/EXAMEN_24MA_PRIMO.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class EXAMEN_24MA_PRIMO {
+
+	public static void main(String[] args) {
+		Scanner ent = new Scanner(System.in);
+	
+		System.out.println("Buenos dias Ceinmark, soy Carlos Antony");
+		System.out.println("Introduce un numero para averiguar si es primo o no :");
+		int num = ent.nextInt();
+	}
+
+}
